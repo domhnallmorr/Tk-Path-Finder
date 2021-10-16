9216bbb6af4ee5a4b91aa58199cfbba986181b2c
Domhnall Morrissey <domhnallmorr@yahoo.co.uk>
2021-10-16 16:53:15 +0100

    V0.08.1 Fixed Bug in Address Bar Enter Bind

--- src/address_bar.py
+++ src/address_bar.py
@@ -14,6 +14,6 @@ class AddressBarEntry(ttk.Entry):
 		self.insert(0, self.branch_tab.explorer.current_directory) #inserts new value assigned by 2nd parameter
 		
 	def enter_event(self, event):
-		self.branch_tab.explorer.address_bar_updateed(self.get())
-		self.branch_tab.update_tab()
+		#self.branch_tab.explorer.address_bar_updateed(self.get())
+		self.branch_tab.update_tab(self.get())
 		self.update_bar()
