6d6a28b987b8e5525607ab1ca5793f1fb97cf72c
Domhnall Morrissey <domhnallmorr@yahoo.co.uk>
2021-11-06 13:49:49 +0000

    V0.20.1 Added cntrl -d bind to create folder window, this duplicates the current line

--- src/branch_tab.py
+++ src/branch_tab.py
@@ -437,6 +437,23 @@ class AddFoldersWindow(ttk.Frame):
 		self.ok_btn.grid(row=2, column=0, padx=5, pady=5, sticky='ne')
 		self.cancel_btn = ttk.Button(self.top, text='Cancel', width=10, style='danger.TButton', command=lambda button='cancel': self.cleanup(button))
 		self.cancel_btn.grid(row=2, column=1, padx=5, pady=5, sticky='nw')
+		self.folder_text.bind('<Control-d>', self.duplicate_line)
+		
+	def duplicate_line(self, event):
+		current_line = int(self.folder_text.index(INSERT).split('.')[0])-1 #convert to 0 index system
+		all_lines = self.folder_text.get("1.0",END).split('\n')
+		
+		all_lines.insert(current_line+1, all_lines[current_line])
+		#all_lines = '\n'.join(all_lines)
+		lines_to_insert = []
+		
+		for l in all_lines:
+			if not l.replace('\n', '').strip() == '': #remove any blank lines
+				lines_to_insert.append(l) 
+
+		self.folder_text.delete('1.0', END)
+		self.folder_text.insert("end", '\n'.join(lines_to_insert))
+		#self.folder_text.mark_set("insert", "%d.%d" % (current_line+1, 1))
 		
 	def cleanup(self, button):
 		if button == 'ok':
