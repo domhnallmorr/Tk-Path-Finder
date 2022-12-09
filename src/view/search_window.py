c74444b38c7f53e0d960fa6eda4c166870196537
Domhnall Morrissey <domhnallmorr@yahoo.co.uk>
2022-12-09 10:58:33 +0000

    V0.42.2 Minor Improvements to Search Window

--- src/view/search_window.py
+++ src/view/search_window.py
@@ -31,36 +31,33 @@ class SearchWindow(ttk.Frame):
 		self.results_frame.grid_columnconfigure(0, weight=1)
 		
 		# Widgets
-		# ttk.Label(self.options_frame, text='Search Type:').grid(row=0, column=0, padx=5, pady=5)
-		# self.search_type_combo = ttk.Combobox(self.options_frame, values=['Raw Text', 'Links'], state='readonly')
-		# self.search_type_combo.set('Raw Text')
-		# self.search_type_combo.config(state='disabled')
-		# self.search_type_combo.grid(row=0, column=1, padx=5, pady=5, sticky='W')
 		
 		ttk.Label(self.options_frame, text='Search For:').grid(row=1, column=0, padx=5, pady=5)
-		self.search_for_combo = ttk.Combobox(self.options_frame, values=['Files', 'Folders'], state='readonly')
+		self.search_for_combo = ttk.Combobox(self.options_frame, width=30, values=['Files', 'Folders'], state='readonly')
 		self.search_for_combo.set('Files')
 		self.search_for_combo.grid(row=1, column=1, padx=5, pady=5, sticky='W')
 
 		ttk.Label(self.options_frame, text='Search Where:').grid(row=2, column=0, padx=5, pady=5)
-		self.search_where_combo = ttk.Combobox(self.options_frame, values=['Parent Directory', 'Parent and Sub Directories'], state='readonly')
+		self.search_where_combo = ttk.Combobox(self.options_frame, width=30, values=['Parent Directory', 'Parent and Sub Directories'], state='readonly')
 		self.search_where_combo.set('Parent Directory')
-		self.search_where_combo.grid(row=2, column=1, padx=5, pady=5, sticky='EW')
+		self.search_where_combo.grid(row=2, column=1, padx=5, pady=5, sticky='W')
 		
 		ttk.Label(self.options_frame, text='Text:').grid(row=3, column=0, padx=5, pady=5)
 		self.search_bar = ttk.Entry(self.options_frame, width=50)
 		self.search_bar.grid(row=3, column=1, padx=5, pady=5)
-
+		self.search_bar.bind('<Return>', self.on_search)
+		
 		ttk.Label(self.options_frame, text='File Extension:').grid(row=4, column=0, padx=5, pady=5)
 		self.extension_extry = ttk.Entry(self.options_frame, width=50)
 		self.extension_extry.grid(row=4, column=1, padx=5, pady=5)
+		self.extension_extry.bind('<Return>', self.on_search)
 		
 		self.python_raw_string = IntVar()
-		ttk.Checkbutton(self.options_frame, text="Python Raw String List Output", variable=self.python_raw_string).grid(row=1, column=2, sticky='w', padx=5, pady=5)
+		ttk.Checkbutton(self.options_frame, text="Python Raw String List Output", variable=self.python_raw_string).grid(row=5, column=1, columnspan=1, sticky='w', padx=5, pady=5)
 
-		b1 = ttk.Button(self.options_frame, text='Search', 
+		b1 = ttk.Button(self.options_frame, text='Search', width=10,
 					command=self.on_search, style='primary.TButton')
-		b1.grid(row=5, column=0, sticky='ew', pady=5, ipadx=10)
+		b1.grid(row=6, column=1, sticky='e', padx=5, pady=5, ipadx=10)
 		
 		# Raw Text Widget
 		self.search_text = tk.Text(self.results_frame, width=110, height=20)
@@ -70,7 +67,7 @@ class SearchWindow(ttk.Frame):
 		vsb.grid(row=1, column=8, sticky='NSEW')
 		self.search_text.configure(yscrollcommand=vsb.set)
 		
-	def on_search(self):
+	def on_search(self, event=None):
 		#results = []
 		self.search_text.delete('1.0', END)
 		
